2b44ef5d18427a0089c6062460377d28d5f68db7
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-06-20 00:02:06 -0500

    classes js

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,11 @@
 console.log("TS test! ! ! ! !");
+var User = /** @class */ (function () {
+    function User(email, name) {
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+    return User;
+}());
+var personA = new User("xxx@xx.com", "xxx");
+personA.city = "shenyang";
