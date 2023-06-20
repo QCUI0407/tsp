d5d53a8aa16b5bbcf1f0fbaae26a606f80b45045
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-06-19 23:59:51 -0500

    classes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 console.log("TS test! ! ! ! !");
+
+class User{
+    email:string
+    name:string
+    city:string = ""
+    constructor(email: string,name:string){
+        this.email = email;
+        this.name =name
+    }
+}
+
+const personA = new User("xxx@xx.com","xxx")
+
+personA.city = "shenyang"
