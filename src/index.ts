44e81150b9b571e6bb16d15849a6c5cb8da37e25
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-06-21 22:53:13 -0500

    private & public

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,31 @@
 console.log("TS test! ! ! ! !");
 
 class User{
-    email:string
+    public email:string
     name:string
     city:string = ""
+    private readonly code: string = "123456"
     constructor(email: string,name:string){
         this.email = email;
         this.name =name
+        this.city
+    }
+}
+
+class UserOne{
+
+    readonly code: string = "123456"
+    constructor(
+        public email: string,
+        public name: string,
+        private userId: string){
+
     }
 }
 
 const personA = new User("xxx@xx.com","xxx")
+
+const personb = new User("xxx@xx.com","xxx")
+
 
 personA.city = "shenyang"
