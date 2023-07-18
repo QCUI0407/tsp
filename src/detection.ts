63441de665af52f5cc0cee20d39a4d958e251b6d
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-07-17 22:10:06 -0500

    narrowing

--- src/detection.ts
+++ src/detection.ts
@@ -12,3 +12,20 @@ function provideId(id:string | null){
     }
     id.toLowerCase()
 }
+
+interface User{
+    name:string,
+    email:string
+}
+
+interface Admin{
+    name:string,
+    email:string,
+    isAdmin:boolean
+}
+
+function adminAccpunt(account: User |Admin){
+    if("isAdmin" in account){
+        return account.isAdmin
+    }
+}
