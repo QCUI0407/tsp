cc9c53a221e817dda5bea4550a0dda564336c6b8
cui-homestudy <cuiqiyuan1989@gmail.com>
2023-07-25 22:11:32 -0500

    Exhaustiveness checking

--- src/detection.ts
+++ src/detection.ts
@@ -73,3 +73,27 @@ function logValue(x: Date | string) {
     width:number
 
   }
+
+  type Shape = Circle | Square | Rectangle
+
+  function getTrueShape(shape: Shape){
+    if (shape.kind === "circle"){
+      return Math.PI * shape.radius ** 2
+    }
+    // return shape.side * shape.side
+  }
+
+  function getArea(shape: Shape){
+    switch(shape.kind){
+      case "circle":
+        return Math.PI * shape.radius ** 2
+      case "square":
+        return shape.side * shape.side
+      case "rectangle":
+        return shape.length * shape.width
+      
+      default:
+        const _defaultforshape: never = shape
+        return _defaultforshape
+      }
+  }
